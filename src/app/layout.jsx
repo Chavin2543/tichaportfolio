880ad39285578824bf20c333721d169092f42352
Chavin Panicharoen <rungallday@hotmail.com>
2024-02-02 00:04:09 +0700

    fix tab text

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,11 +5,11 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    template: '%s - SME to the Moon',
+    default: 'SME to the Moon - for small businesses.',
   },
   description:
-    'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
+    'for small businesses.',
 }
 
 const inter = Inter({
